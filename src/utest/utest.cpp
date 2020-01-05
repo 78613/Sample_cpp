108570de29e4dadef406d2744d18fce347fb9d97
Omar Cardona <omarcardona@yahoo.com>
2020-01-05 15:11:57 -0800

    some initialzer tests

--- src/utest/utest.cpp
+++ src/utest/utest.cpp
@@ -2,6 +2,32 @@
 #include <utest.hpp>
 #include <test.hpp>
 
+typedef struct bar {
+    int       a;
+    int       b;
+    long long c;
+} bar_t;
+
+typedef enum why {
+    zero,
+    one,
+    two,
+    three,
+    four
+} why_t;
+
+void
+test_init(void)
+{
+    bar_t b = {1};
+    why_t w = {};
+    //why_t w = {two};
+
+	std::cout << "Here!" << std::endl;
+	std::cout << w << std::endl;
+    return;
+} //test_init()
+
 int
 main(void)
 {
@@ -9,6 +35,8 @@ main(void)
 
 	lib_active();
 	std::cout << "Hello World!" << std::endl;
+
+    test_init();
 
 	return rc;
 }//main()
